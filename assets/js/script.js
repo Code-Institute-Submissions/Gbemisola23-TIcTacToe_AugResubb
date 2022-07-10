fcb931b122145ba7f471f7b78c30e63296ee1b37
Gbemisola Ajebeku <gbemisolaje@gmail.com>
2022-07-10 16:44:27 +0000

    edit javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
  /* jshint esversion: 6 */
 /* globals $:false */
 
-let playerText = document.getElementById('playerText')
-let cells = document.querySelectorAll('.cell')
-let winnerIndicator = getComputedStyle(document.body).getPropertyValue( '--winning-blocks')
-cells = Array.from(cells)
-let currentPlayer = "x"
-const restarBtn = document.getElementById('restarBtn')
+let playerText = document.getElementById('playerText');
+let cells = document.querySelectorAll('.cell');
+cells = Array.from(cells);
+let currentPlayer = "x";
+const restarBtn = document.getElementById('restarBtn');
 
 const winningCombinations =[
     [0,1,2],
@@ -17,61 +16,54 @@ const winningCombinations =[
     [2,5,8],
     [0,4,8],
     [2,4,6],
-]
+];
 
 function checkForWinner(){
+  "use strict";
     winningCombinations.forEach(function(combination){
-        let check = combination.every(idx => cells[idx].innerText.trim() == currentPlayer)
+        let check = combination.every(idx => cells[idx].innerText.trim() == currentPlayer);
     if(check){
         highlightCells(combination);
         playerText.innerHTML =`${currentPlayer} has won!`;
     }
-  
-  })
+  });
 }
 
-
-
 function highlightCells(combination){
+  "use strict";
 combination.forEach(function(idx){
   cells[idx].classList.add("highlight");
-})
+});
 }
 
 cells.forEach(function(cell){
+  "use strict";
     cell.addEventListener('click', function(){
         if(cell.innerText.trim() != "")return;
-       cell.innerText = currentPlayer
-
+       cell.innerText = currentPlayer;
+       checkForWinner();
+    
        if(playerHasWon()!== false){
-        playerText.innerHTML =`${currentPlayer}has won!`
-        let winning_blocks = playerHasWon() 
-     }
-      
-       currentPlayer = currentPlayer == "x" ? "o" : "x"
-       checkForWinner()
-    })
-})
+        playerText.innerHTML =`${currentPlayer}has won!`;
+        }
+       currentPlayer = currentPlayer == "x" ? "o" : "x";
+    });
+});
 
 function playerHasWon(){
+  "use strict";
      for (const condition of winningCombinations) {
-        let[a,b,c] = condition
+        let[a,b,c] = condition;
         if (cells[a] && cells[a] == cells[b] && cells[a]== cells[c]){
-            return[a,b,c]
+            return[a,b,c];
         }
-
      }
-     return false
+     return false;
 }
 
-restarBtn.addEventListener('click', Restart)
+restarBtn.addEventListener('click', Restart);
 
 function Restart() {
-   location.reload()
-
+  "use strict";
+   location.reload();
 }
-
- 
-  
-  
-  
